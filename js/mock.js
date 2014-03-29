1769127331210640cf18fe480f9ae93e0d6eed45
oprysok <mykhaylo.oprysok@gmail.com>
2014-03-29 16:41:49 +0200

    Mocking XHR globally.

--- js/mock.js
+++ js/mock.js
@@ -1,25 +1,24 @@
-Widget.prototype.detect = function() 
-{ 
-	if (typeof this.locationData == "undefined") this.mockLocationData();
-	this.checkLidok(this.locationData, this);
-};
+if (typeof System == "undefined" || typeof System.Gadget == "undefined") {
 
-Widget.prototype.mockLocationData = function(direction, area, timestamp)
-{
-	var timeStampDefault = "2014\/03\/27 20:43:00";
-	var directionDefault = "in";
-	var areaDefault = this.settings.homeArea;
-	if (typeof direction != "undefined") directionDefault = direction;
-	if (typeof area != "undefined") areaDefault = area;
-	if (typeof area != "undefined") areaDefault = area;
-	this.locationData = "{\"timestamp\":\""+timeStampDefault+"\",\"locationid\":95,\"direction\":\""+directionDefault+"\",\"area\":\""+areaDefault+"\",\"working\":true}";
-	this.employeeList = [{"id":2531,"last_name":"Klym","first_name":"Taras"},
-	{"id":3116,"last_name":"Klymchuk","first_name":"Liudmyla"},
-	{"id":2323,"last_name":"Klymenko","first_name":"Andrii"},
-	{"id":3971,"last_name":"Mykhailov","first_name":"Maksym"},
-	{"id":3987,"last_name":"Mykhailov","first_name":"Volodymyr"},
-	{"id":2346,"last_name":"Mykhailov","first_name":"Volodymyr"},
-	{"id":3957,"last_name":"Nedokushev","first_name":"Mykhailo"},
-	{"id":3360,"last_name":"Monchak","first_name":"Mykhailo"},
-	{"id":1963,"last_name":"Chmykhalo","first_name":"Pavlo"}];
-};
+	System = {};
+
+	System.Gadget = {};
+
+	mockXHR.mock([
+	 	{
+	 	url:"http://poc-cont2-srv/polygon/json/employees", 
+	 	status:"200", 
+	 	responseText:"[{\"id\":2531,\"last_name\":\"Klym\",\"first_name\":\"Taras\"},{\"id\":2323,\"last_name\":\"Klymenko\",\"first_name\":\"Andrii\"},{\"id\":3971,\"last_name\":\"Mykhailov\",\"first_name\":\"Maksym\"},{\"id\":3116,\"last_name\":\"Klymchuk\",\"first_name\":\"Liudmyla\"},{\"id\":3987,\"last_name\":\"Mykhailov\",\"first_name\":\"Volodymyr\"},{\"id\":2346,\"last_name\":\"Mykhailov\",\"first_name\":\"Volodymyr\"},{\"id\":3957,\"last_name\":\"Nedokushev\",\"first_name\":\"Mykhailo\"},{\"id\":3360,\"last_name\":\"Monchak\",\"first_name\":\"Mykhailo\"},{\"id\":1963,\"last_name\":\"Chmykhalo\",\"first_name\":\"Pavlo\"}]"
+		},
+	 	{
+	 	url:"http://poc-cont2-srv/polygon/json/last_seen?employeeId=*",
+	 	status:"200", 
+	 	responseText:JSON.stringify({
+	 		"timestamp":"2014/03/27 20:43:00",
+	 		"locationid":95,
+	 		"direction":"out",
+	 		"area":"Office KBP3-L",
+	 		"working":true})
+		}
+	]);
+}
