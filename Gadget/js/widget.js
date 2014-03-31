703d1dd2dfd757e3a7a4ced5821893fb30bd660b
oprysok <mykhaylo.oprysok@gmail.com>
2014-03-31 22:11:17 +0300

    This fixes issue #2 and improves code quality.

--- Gadget/js/widget.js
+++ Gadget/js/widget.js
@@ -20,7 +20,7 @@ function Widget() {
         "Office KBP5-C",
         "Office KBP5-G",
         "Office KBP5-L",
-        "Office KBP5-R",
+        "Office KBP5-R"
     ];
     this.settings = {
         "interval": 3000,
@@ -59,14 +59,13 @@ function Widget() {
 }
 Widget.prototype.changeState = function (detected) {
     "use strict";
-    //debugger;
     if (detected) {
         if (this.lastResult === null || !this.lastResult) {
             this.lastResult = true;
             this.playSound(this.audio.near);
         }
         this.dom.backgroundEl.style.background = "#e74c3c";
-        this.dom.statusSpan.innerHTML = this.settings.alias != "" ? this.settings.alias + " detected!" : "Detected!";
+        this.dom.statusSpan.innerHTML = this.settings.alias !== "" ? this.settings.alias + " detected!" : "Detected!";
 
     } else {
         if (this.lastResult === null || this.lastResult) {
@@ -94,7 +93,6 @@ Widget.prototype.checkLidok = function (obj, that) {
     that.dom.locationSpan.innerHTML = res.direction === "in" ? "Now in " +  that.currentLocationName : "Last seen in " + that.currentLocationName;
     that.dom.userSpan.innerHTML = that.settings.name;
     that.dom.whenSpan.innerHTML = res.direction !== "in" ? "(" + moment(res.timestamp).fromNow() + ")" : "";
-    //debugger;
     if (res.area === that.settings.homeArea && res.direction === "in") {
         that.changeState(true);
     } else {
@@ -115,26 +113,31 @@ Widget.prototype.playSound = function (filepath) {
     }
 };
 Widget.prototype.closeSuggestions = function () {
+    "use strict";
     this.dom.employeesInputAc.style.border = "";
-    this.dom.employeesSuggestions.style.display= "none";
+    this.dom.employeesSuggestions.style.display = "none";
     this.dom.employeesSuggestions.innerHTML = "";
 };
 Widget.prototype.openSuggestions = function () {
+    "use strict";
     this.dom.employeesInputAc.style.borderRight = "2px solid #996600";
-    this.dom.employeesSuggestions.style.display= "block";
+    this.dom.employeesSuggestions.style.display = "block";
 };
 Widget.prototype.closeSettings = function () {
-    document.body.setAttribute("data-showSettings","false");
-    document.body.style.height = 58 +"px";
-    this.dom.settings.style.display = "none";    
+    "use strict";
+    document.body.setAttribute("data-showSettings", "false");
+    document.body.style.height = 58 + "px";
+    this.dom.settings.style.display = "none";
 };
 Widget.prototype.openSettings = function () {
+    "use strict";
     this.fillSettings();
-    document.body.setAttribute("data-showSettings","true");
+    document.body.setAttribute("data-showSettings", "true");
     document.body.style.height = 58 + 58 + 58 + 58 + 58 + "px";
-    this.dom.settings.style.display = "block";   
+    this.dom.settings.style.display = "block";
 };
 Widget.prototype.fillSettings = function () {
+    "use strict";
     this.dom.employeesInputAc.value = this.settings.name;
     this.dom.employeesInputId.value = this.settings.userId;
     this.dom.aliasInput.value = this.settings.alias;
@@ -143,37 +146,37 @@ Widget.prototype.fillSettings = function () {
     this.dom.muteInput.checked = this.settings.mute;
 };
 Widget.prototype.saveSettings = function () {
+    "use strict";
     if (this.dom.employeesInputId.value) {
-        this.settings.name = this.dom.employeesInputAc.value;
-        this.settings.userId = parseInt(this.dom.employeesInputId.value);
+        this.settings.name = this.dom.employeesInputId.getAttribute("data-name");
+        this.settings.userId = parseInt(this.dom.employeesInputId.value, 10);
     }
     this.settings.alias = this.dom.aliasInput.value;
     this.settings.homeArea = this.dom.areaInput.value;
     if (!isNaN(this.dom.intervalInput.value)) {
-        this.settings.interval = parseInt(this.dom.intervalInput.value);
+        this.settings.interval = parseInt(this.dom.intervalInput.value, 10);
     }
     this.settings.mute = this.dom.muteInput.checked;
     this.closeSuggestions();
 };
 //Event handlers
-Widget.prototype.autocompleteHandler = function (event,that) {
+Widget.prototype.autocompleteHandler = function (event, that) {
+    "use strict";
     var keyCode, str, found, i, y, lName, fName, charCount;
-    keyCode = ('which' in event) ? event.which : event.keyCode;
-    if (keyCode === 40) {
-        //handle down key - to be implemented
-    } else if (keyCode === 27) {
+    keyCode = (event !== undefined) ? event.which : event.keyCode;
+    if (keyCode === 27) {
         this.closeSuggestions();
     } else {
         charCount = that.value.toLowerCase().length;
-        str = that.value.toLowerCase().replace(/^\s+|\s+$/g,"").split(" "); // replace is used to trim string. String.trim() in old ie not supported
-        if (str[0]!=='' && charCount > 3) {
+        str = that.value.toLowerCase().replace(/^\s+|\s+$/g, "").split(" "); // replace is used to trim string. String.trim() in old ie not supported
+        if (str[0] !== '' && charCount > 3) {
             this.dom.employeesSuggestions.innerHTML = "";
             for (i = 0; i < this.employeeList.length; i += 1) {
                 lName = this.employeeList[i].last_name !== null ? this.employeeList[i].last_name.toLowerCase() : "empty";
                 fName = this.employeeList[i].first_name !== null ? this.employeeList[i].first_name.toLowerCase() : "empty";
                 found = true;
                 for (y = 0; y < str.length; y += 1) {
-                    if (lName.indexOf(str[y]) !==-1 || fName.indexOf(str[y]) !==-1) {
+                    if (lName.indexOf(str[y]) !== -1 || fName.indexOf(str[y]) !== -1) {
                         found = true;
                     } else {
                         found = false;
@@ -181,7 +184,7 @@ Widget.prototype.autocompleteHandler = function (event,that) {
                     }
                 }
                 if (found) {
-                    this.dom.employeesSuggestions.innerHTML += "<div onmouseleave=\"return widget.onSuggestionOut(event, this);\" onmouseover=\"return widget.onSuggestionHover(event, this);\" onclick=\"return widget.suggestionClickHandler(event, this);\" class=\"suggestion\" data-id=\"" + this.employeeList[i].id  + "\">"+this.employeeList[i].first_name + " " + this.employeeList[i].last_name + "</div><br/>";
+                    this.dom.employeesSuggestions.innerHTML += "<div onmouseleave=\"return widget.onSuggestionOut(this);\" onmouseover=\"return widget.onSuggestionHover(this);\" onclick=\"return widget.suggestionClickHandler(this, " + this.employeeList[i].id + ", '" + this.employeeList[i].first_name + " " + this.employeeList[i].last_name + "');\" class=\"suggestion\" data-id=\"" + this.employeeList[i].id  + "\">" + this.employeeList[i].first_name + " " + this.employeeList[i].last_name + "</div><br/>";
                 }
             }
             if (this.dom.employeesSuggestions.innerHTML !== "") {
@@ -191,48 +194,55 @@ Widget.prototype.autocompleteHandler = function (event,that) {
             }
         } else {
             this.closeSuggestions();
-            this.dom.employeesInputId.value = this.settings.userId;
         }
     }
     this.changesMade();
 };
-Widget.prototype.suggestionClickHandler = function (event,that) {
-    this.dom.employeesInputId.value = that.getAttribute("data-id");
+Widget.prototype.suggestionClickHandler = function (that, id, name) {
+    "use strict";
+    this.dom.employeesInputId.value = parseInt(id, 10);
+    this.dom.employeesInputId.setAttribute("data-name", name);
     this.dom.employeesInputAc.value = that.innerHTML;
     this.closeSuggestions();
     this.changesMade();
 };
-Widget.prototype.onCogHover = function (event,that) {
+Widget.prototype.onCogHover = function (that) {
+    "use strict";
     that.style.backgroundPosition = "-22px 0";
 };
-Widget.prototype.onCogOut = function (event,that) {
+Widget.prototype.onCogOut = function (that) {
+    "use strict";
     that.style.backgroundPosition = "0 0";
 };
-Widget.prototype.onSuggestionHover = function (event,that) {
+Widget.prototype.onSuggestionHover = function (that) {
+    "use strict";
     that.style.background = "#16a085";
 };
-Widget.prototype.onSuggestionOut = function (event,that) {
+Widget.prototype.onSuggestionOut = function (that) {
+    "use strict";
     that.style.background = "";
 };
 Widget.prototype.changesMade = function () {
-    if (this.settings.alias === this.dom.aliasInput.value && this.settings.userId === parseInt(this.dom.employeesInputId.value) && this.settings.homeArea === this.dom.areaInput.value && this.settings.interval === parseInt(this.dom.intervalInput.value) && this.settings.mute === this.dom.muteInput.checked) {
+    "use strict";
+    if (this.settings.alias === this.dom.aliasInput.value && this.settings.userId === parseInt(this.dom.employeesInputId.value, 10) && this.settings.homeArea === this.dom.areaInput.value && this.settings.interval === parseInt(this.dom.intervalInput.value, 10) && this.settings.mute === this.dom.muteInput.checked) {
         this.dom.saveSpan.className = "disabled";
     } else {
         this.dom.saveSpan.className = "";
     }
 };
-Widget.prototype.onSaveClick = function (event,that) {
+Widget.prototype.onSaveClick = function (that) {
+    "use strict";
     if (that.className !== "disabled") {
         this.saveSettings();
         this.closeSettings();
-        this.dom.saveSpan.className = "disabled";       
+        this.dom.saveSpan.className = "disabled";
     }
 };
 Widget.prototype.onSettingsClick = function () {
     "use strict";
-    if (document.body.getAttribute("data-showSettings")==="true") {
+    if (document.body.getAttribute("data-showSettings") === "true") {
         this.closeSettings();
     } else {
         this.openSettings();
-    };
+    }
 };
